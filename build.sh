e7a6ee4c7f0a7d07bd300bdf2af034c3163f8815
Davis Goodin <dagood@microsoft.com>
2020-06-10 16:29:28 -0500

    rm hard-coded cache isolation: no longer dangerous

--- build.sh
+++ build.sh
@@ -17,6 +17,5 @@ scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
-export NUGET_PACKAGES="$scriptroot/packages/restored/"
 
 "$scriptroot/eng/common/build.sh" --build --restore --pack "$@"
